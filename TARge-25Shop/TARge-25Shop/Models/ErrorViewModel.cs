38dcbe8a8bf01c5acd5c4f3803ddda118cf78a23
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-01 09:35:44 +0300

    01.09.2026 Shop ülesande alus

---
+++ TARge-25Shop/TARge-25Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TARge_25Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
