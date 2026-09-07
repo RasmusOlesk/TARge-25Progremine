3e0fdfc1fd0108a5fd67d5587e3bc5a9e1777a39
jussi2000 <johannat2009@gmail.com>
2026-09-07 15:27:15 +0300

    07.09.2026 TARge25 application, core, data  intial creation

---
+++ TARge-25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TARge25Shop.Data
+{
+    
+    public class TARge25ShopContext : DbContext
+    {
+
+        //see class tuleb teha DbContextiks,
+        //et saaks kasutada Entity Frameworki andmebaasi operatsioone
+
+    }
+}
